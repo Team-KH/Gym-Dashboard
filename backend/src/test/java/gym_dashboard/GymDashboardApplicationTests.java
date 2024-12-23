a31b14be0b5f7d5bea170e7eef9e5db0acf6364b
Alaa-Khalaila <alkhalila.alaa@gmail.com>
2024-12-24 02:00:56 +0300

    adding backend

---
+++ backend/src/test/java/gym_dashboard/GymDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package gym_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

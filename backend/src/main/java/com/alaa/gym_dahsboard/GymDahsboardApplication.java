1b011874841198f5e465d7d06267dfa932020226
Alaa-Khalaila <alkhalila.alaa@gmail.com>
2025-04-09 05:22:05 +0300

    update backend files

---
+++ backend/src/main/java/com/alaa/gym_dahsboard/GymDahsboardApplication.java
@@ -0,0 +1,13 @@
+package com.alaa.gym_dahsboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymDahsboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymDahsboardApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/alaa/gym_dahsboard/GymDahsboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alaa.gym_dahsboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymDahsboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
